e4d69ce2c538a8a74cac7cf6c6acdec0844f1fef
aleksaad4 <aleksaad4@gmail.com>
2016-12-29 11:20:33 +0300

    Initial commit

---
+++ src/main/java/ad4si2/lfp/LfpApplication.java
@@ -0,0 +1,12 @@
+package ad4si2.lfp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfpApplication.class, args);
+	}
+}
---
+++ src/test/java/ad4si2/lfp/LfpApplicationTests.java
@@ -0,0 +1,16 @@
+package ad4si2.lfp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LfpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
